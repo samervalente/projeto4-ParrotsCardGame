42ea18356950093a14ace9f7d98dff5091952245
Samer Valente <samervalente@gmail.com>
2022-04-09 13:35:27 -0300

    Última versão - Virar cartas

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 
 //Validação do input
-let quantidade = prompt("Qual a quantidade de cartas?")
+let quantidade = prompt("Digite uma quantidade de cartas entre 4 e 14")
 
 while(quantidade < 4 || quantidade > 14 || quantidade % 2 != 0  ){
-    quantidade = prompt("Qual a quantidade de cartas?") 
+    quantidade = prompt("Valor Inválido. Digite uma quantidade par de cartas entre 4 e 14 ") 
 }
 
 //Banco de imagens
@@ -18,13 +18,12 @@ function comparador() {
 }
 
 imagebank.sort(comparador)
-
 function getCartas(){
     cartas.length = quantidade
     imagebank.length = quantidade/2
     for(let i = 0; i < imagebank.length; i ++){
         card = `
-        <div class="cartao" onclick="virarcarta(this)">
+        <div class="carta ${imagebank[i]}"  onclick="virarcarta(this)">
             <div class="face front-face"><img src="images/front.png"></div>
             <div class="face back-face"><img src="images/${imagebank[i]}"></div> 
         </div>`
@@ -37,18 +36,32 @@ function getCartas(){
 function adicionaCartas(){
     let container = document.querySelector(".container")
    
-    container.innerHTML = cartas
+    container.innerHTML = cartas.join('')
 }
 
 getCartas()
 adicionaCartas()
-console.log(pares)
-console.log(cartas)
 
 function virarcarta(carta){
+    let caixaSelecionada  = document.querySelector(".clicada")
+    giracarta(carta) 
+    carta.classList.add("clicada")
+   
+    verifica(carta, caixaSelecionada)
+  
+}
+function giracarta(carta){
     carta.querySelector(".front-face").classList.add("rotacionafront")
-    carta.querySelector(".back-face").classList.add("rotacionavoltaback")
+    carta.querySelector(".back-face").classList.add("rotacionavoltaback") 
+}
+
+function verifica(carta, caixaSelecionada){
+    if(carta.innerHTML === caixaSelecionada.innerHTML){
+        console.log("Iguais")
+    }else if(carta.innerHTML !== caixaSelecionada.innerHTML){
+        console.log("Diferentes")
+    }
+
 }
 
 
-   
